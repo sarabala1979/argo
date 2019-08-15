b140f592e63490e0a91437226723d50923cae1b1
Saravanan Balasubramanian <sarabala1979@gmail.com>
2019-08-15 11:53:30 -0700

    udated generate-proto.sh

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -17,22 +17,37 @@ PATH="${PROJECT_ROOT}/dist:${PATH}"
 go build -i -o dist/go-to-protobuf ./vendor/k8s.io/code-generator/cmd/go-to-protobuf
 go build -i -o dist/protoc-gen-gogo ./vendor/k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo
 
+cp dist/protoc-gen-gogo $GOPATH/bin
+
 # Generate pkg/apis/<group>/<apiversion>/(generated.proto,generated.pb.go)
 # NOTE: any dependencies of our types to the k8s.io apimachinery types should be added to the
 # --apimachinery-packages= option so that go-to-protobuf can locate the types, but prefixed with a
 # '-' so that go-to-protobuf will not generate .proto files for it.
 PACKAGES=(
     github.com/argoproj/argo/pkg/apis/workflow/v1alpha1
+    k8s.io/apimachinery/pkg/apis/meta/v1
+    k8s.io/apimachinery/pkg/util/intstr
+    k8s.io/apimachinery/pkg/api/resource
+    k8s.io/api/core/v1
 )
 APIMACHINERY_PKGS=(
-    +k8s.io/apimachinery/pkg/util/intstr
-    +k8s.io/apimachinery/pkg/api/resource
+    k8s.io/apimachinery/pkg/apis/meta/v1
+    k8s.io/apimachinery/pkg/util/intstr
+    k8s.io/apimachinery/pkg/api/resource
+    k8s.io/api/core/v1
     +k8s.io/apimachinery/pkg/runtime
-    +k8s.io/apimachinery/pkg/apis/meta/v1
-    +k8s.io/api/core/v1
 )
+IMPROT_PKG=(
+    /Users/sbalasubram/go/src/github.com/argoproj/argo/vendor/k8s.io/apimachinery/pkg/apis/meta/v1
+    /Users/sbalasubram/go/src/github.com/argoproj/argo/vendor/k8s.io/apimachinery/pkg/util/intstr
+    /Users/sbalasubram/go/src/github.com/argoproj/argo/vendor/k8s.io/apimachinery/pkg/api/resource
+   /Users/sbalasubram/go/src/ github.com/argoproj/argo/vendor/k8s.io/api/core/v1
+    /Users/sbalasubram/go/src/k8s.io/apimachinery/pkg/runtime
+)
+
 go-to-protobuf \
     --go-header-file=${PROJECT_ROOT}/hack/custom-boilerplate.go.txt \
     --apimachinery-packages=$(IFS=, ; echo "${APIMACHINERY_PKGS[*]}") \
     --packages=$(IFS=, ; echo "${PACKAGES[*]}") \
-    --proto-import=./vendor
+    --proto-import=./vendor \
+    --proto-import=$(IFS=, ; echo "${IMPROT_PKG[*]}")
