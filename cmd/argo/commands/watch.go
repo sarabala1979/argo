031fe8c3c9314a832664f07c7c63e2b6583e038a
Saravanan Balasubramanian <sarabala1979@gmail.com>
2020-07-20 15:12:30 -0700

    fix: Panic on CLI Watch command

--- cmd/argo/commands/watch.go
+++ cmd/argo/commands/watch.go
@@ -93,7 +93,7 @@ func watchWorkflow(ctx context.Context, serviceClient workflowpkg.WorkflowServic
 		}
 
 		printWorkflowStatus(wf, getArgs)
-		if !wf.Status.FinishedAt.IsZero() {
+		if wf != nil && !wf.Status.FinishedAt.IsZero() {
 			return
 		}
 	}
